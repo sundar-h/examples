3e798a5e994129aaeb2f8fbdd7ef82926d2391d0
sanghaifa <sanghaifa@sensetime.com>
2021-09-23 18:00:01 +0800

    2021-09-23

---
+++ rust_async/src/main.rs
@@ -0,0 +1,15 @@
+// 异步编程练习
+// 本节目标
+// 自定义实现Feature(一次异步值), Stream(可重复的异步值), Sink(一次或多次异步值)
+
+fn main() {
+    println!("Hello, world!");
+    //     std::future::Future;
+    //     std::stream::Stream;
+    //     // std::io::Sink
+
+    //     futures::future::Future;
+    //     futures::Future;
+    //     futures::Stream;
+    //     futures::Sink;
+}
