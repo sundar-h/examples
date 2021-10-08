04706d7c05a7defc8b18029ab357cfd5652f9b10
sanghaifa <sanghaifa@sensetime.com>
2021-10-08 17:55:58 +0800

    2020-10-08

---
+++ rust/enum_option_result/examples/option.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct A {
+    name: String,
+}
+
+fn main() {
+    let a = A {
+        name: String::from("lalala"),
+    };
+    let a = Some(a);
+
+    // ----------------------------------------------
+    // 这里 if let Some a的所有权已经转移
+    // 之后的代码块 已经不能再使用a了
+    // if let Some(i) = a {
+    //     println!("{:?}", i);
+    // }
+
+    // 错误: value borrowed here after partial move
+    // if a.is_none() {
+    //     println!("YES");
+    // }
+    // ----------------------------------------------
+
+    // 正确用法 应该是引用而非借用
+    // if let Some(i) = &mut a
+    if let Some(i) = &a {
+        println!("{:?}", i);
+    }
+
+    // 上面是引用，所以之后a还可以使用 切Option不为None
+    if a.is_none() {
+        println!("YES");
+    }
+
+    // 同上 用法
+    // if let Some(i) = a.as_ref() {
+    //     println!("{:?}", i);
+    // }
+
+    // println!("{:?}", a);
+}
---
+++ rust/rust_async/examples/async_future.rs
@@ -0,0 +1,91 @@
+use futures::future::BoxFuture;
+use futures::{Future, FutureExt};
+use std::pin::Pin;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Waker};
+use std::thread;
+use std::time::Duration;
+
+fn main() {}
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        // 定时器 已经完成 直接返回成功
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            // 定时未结束
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl TimerFuture {
+    pub fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+
+        // 新开线程，定时 唤醒主线程
+        let thread_shared_state = shared_state.clone();
+        thread::spawn(move || {
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake()
+            }
+        });
+
+        TimerFuture { shared_state }
+    }
+}
+
+// 管道接受任务，并执行
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+// 创建新的future, 并发送
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+// 一个可以重新安排自己被Executor调用的future
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    // 将任务放回任务队列
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+    (Executor { ready_queue }, Spawner { task_sender })
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.send(task).expect("too many tasks queued");
+    }
+}
